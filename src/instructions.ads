9ebd2d953ecda6351fea7a4bffc222abf5b78d43
Andrea Gardiman <ale.corny@gmail.com>
2015-09-23 17:27:42 +0200

    Initial structure

---
+++ src/instructions.ads
@@ -0,0 +1,7 @@
+package Instructions is
+
+   -- Type Instruction colleclt all the possible processors instructions.
+   -- At the present moment it is statically defined but in a future work it
+   -- can be
+   type Instruction is (                        );
+end Instructions;
---
+++ src/nodes.ads
@@ -0,0 +1,13 @@
+package Nodes is
+
+   -- Type Node definition
+   type Node is tagged private;
+   type Node_Ptr is access Node;
+
+private
+   -- Type node data-structre
+   type Node is tagged
+      record
+      end record;
+
+end Nodes;
